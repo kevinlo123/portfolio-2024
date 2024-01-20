b67f76f5fb51e2d879156e4988df4b0855aaab15
Kevin Lopez <lopezkevin175@gmail.com>
2024-01-20 03:10:08 -0500

    linking project to github repo

---
+++ src/app/projects/page.js
@@ -0,0 +1,8 @@
+export default function projects() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        testing projects 1
+      </main>
+    );
+  }
+  
