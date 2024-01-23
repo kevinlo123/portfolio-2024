fee875c703078fb198cc2e58d7cf5c7f42ef24e0
Kevin Lopez <lopezkevin175@gmail.com>
2024-01-23 16:23:58 -0500

    fixing import for deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import HomePageHero from './components/HomePageHero.js'
 import TextAnimation from './components/TextAnimation.js'
-import Projects from './components/Projects.js'
+import Projects from './Projects.js'
  
 
 export default function Home() {
