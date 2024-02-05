315fd90ea146158d987f346c44696f83834a0f03
Kevin Lopez <lopezkevin175@gmail.com>
2024-02-05 03:37:21 -0500

    containerized portfolio

--- src/app/components/HomePageHero.js
+++ src/app/components/HomePageHero.js
@@ -8,7 +8,7 @@ export default function HomePageHero () {
           <span className="uppercase font-bold tracking-widest text-xl">resume</span>
         </a>
       </div>
-      <h1 className="overflow-hidden text-heading font-bold text-[45px] leading-[100%] xl:text-[120px] 2xl:text-[150px] xl:leading-[120px] 2xl:leading-[140px] uppercase mb-16">
+      <h1 className="overflow-hidden text-heading font-bold text-[45px] leading-[100%] xl:text-[120px] 2xl:text-[125px] xl:leading-[120px] 2xl:leading-[140px] uppercase mb-16">
         Frontend Developer
       </h1>
       <div className="absolute left-0 bottom-[60px] right-0 flex items-end lg:items-start justify-between px-[30px] lg:px-[60px]">
