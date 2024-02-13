21d4561b288823703d9e69856515bc6a035d04db
Kevin Lopez <lopezkevin175@gmail.com>
2024-02-13 01:32:46 -0500

    fixing document is not defined bug

--- src/app/globals/Header.js
+++ src/app/globals/Header.js
@@ -1,18 +1,20 @@
 'use client';
 
-import Link from 'next/link'
-import LightDarkToggle from '../components/LightDarkToggle.js'
-import { Divide as Hamburger } from 'hamburger-react'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import LightDarkToggle from '../components/LightDarkToggle.js';
+import { Divide as Hamburger } from 'hamburger-react';
 
 export default function Header() {
-  const [isOpen, setOpen] = useState(false)
+  const [isOpen, setOpen] = useState(false);
 
-  if (typeof document !== 'undefined' && isOpen) {
-    document.getElementsByTagName('body')[0].style.overflow = "hidden";
-  } else {
-    document.getElementsByTagName('body')[0].style.overflow = "auto";
-  }
+  useEffect(() => {
+    if (isOpen) {
+      document.getElementsByTagName('body')[0].style.overflow = "hidden";
+    } else {
+      document.getElementsByTagName('body')[0].style.overflow = "auto";
+    }
+  }, [isOpen]);
 
   const close = () => {
     document.getElementsByClassName('hamburger-react')[0].click();
@@ -28,15 +30,13 @@ export default function Header() {
           toggle={setOpen} 
         />
         {
-          isOpen ?
+          isOpen &&
             <nav className="lg:py-[4rem] text-center lg:text-left lg:px-[4rem] py-[3rem] px-[3rem] absolute w-[100%] h-[100%] bg-black left-0 top-0 z-[51] flex flex-col">
               <Link onClick={close} href="/" className="mb-5 text-[40px] tracking-widest lg:text-[60px] font-[300] text-heading">Home</Link>
               <Link onClick={close} href="/about" className="mb-5 text-[40px] tracking-widest lg:text-[60px] font-[300] text-heading">About</Link>
               <Link onClick={close} href="/projects/acoustic" className="mb-5 text-[40px] tracking-widest lg:text-[60px] font-[300] text-heading">Projects</Link>
               <Link onClick={close} href="/contact" className="mb-5 text-[40px] tracking-widest lg:text-[60px] font-[300] text-heading">Contact</Link>
             </nav>
-          :
-          ''
         }
       </div>
     </header>
