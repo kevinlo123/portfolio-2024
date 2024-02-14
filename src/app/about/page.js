eaad54366f83e477968d997d6e76d8e3b9885abe
Kevin Lopez <lopezkevin175@gmail.com>
2024-02-14 00:49:50 -0500

    testing bug fix on server

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,21 +8,21 @@ return (
         <div className="grid gap-4">
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://cdn.pixabay.com/photo/2017/08/28/19/12/international-2690990_640.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center "
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center "
               src="https://i.pinimg.com/736x/e4/7f/d4/e47fd402b110f1aa5633912e5d037171.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://www.in2code.de/fileadmin/_processed_/0/b/csm_code_javascript_49d002a67e.webp"
               alt="gallery-photo"
               />
@@ -31,21 +31,21 @@ return (
         <div className="grid gap-4">
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://i.pinimg.com/originals/69/2b/74/692b74f0046892a053041dd9423afb01.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://trattoriailpanino.com/wp-content/uploads/2022/08/trat43.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center "
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center "
               src="https://media.cnn.com/api/v1/images/stellar/prod/gamestop-black-friday-deals-lead.jpg?q=h_1414,w_2121,x_0,y_0"
               alt="gallery-photo"
               />
@@ -54,21 +54,21 @@ return (
         <div className="grid gap-4">
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/01/boston-skyline-best-views-socail.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center "
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center "
               src="https://media.npr.org/assets/img/2023/11/18/gettyimages-1800093928-3ea884e9fb65ed78aa5a1106a1670342ba8c40cc.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://media.istockphoto.com/id/1307752765/vector/css-web-coding-language-background.jpg?s=612x612&w=0&k=20&c=doA8FdDS9QJghC-4Gkk69WA_5nqVCVjWik-ebUIS144="
               alt="gallery-photo"
               />
@@ -77,14 +77,14 @@ return (
         <div className="grid gap-4">
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://i.pinimg.com/736x/a1/6b/a0/a16ba0d0dc35df6649fcb42430fb0f9d.jpg"
               alt="gallery-photo"
               />
           </div>
           <div>
             <img
-              className="h-[100%] max-w-full rounded-lg object-cover object-center"
+              className="h-auto lg:h-[100%] max-w-full rounded-lg object-cover object-center"
               src="https://i.pinimg.com/736x/5d/ee/a7/5deea7b9a26726235221933f8348fb4f.jpg"
               alt="gallery-photo"
               />
