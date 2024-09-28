2ed8c0e487785b3ee425ccc85ae4a4d086bbfa9c
Kevin Lopez <klopez@thebloc.com>
2024-09-27 23:02:27 -0400

    late 2024 updates

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
 
 export default function About() {
   const [loading, setLoading] = useState(true);
@@ -12,6 +13,8 @@ export default function About() {
     '/about/e47fd402b110f1aa5633912e5d037171.jpg',
     '/about/il_fullxfull.3201439236_f02a.jpg',
     '/about/csm_code_javascript_49d002a67e.webp',
+    '/about/300x600.gif',
+    '/about/trat39.jpg',
     '/about/692b74f0046892a053041dd9423afb01.jpg',
     '/about/trat43.jpg',
     '/about/gamestop-black-friday-deals-lead.jpg',
@@ -22,11 +25,18 @@ export default function About() {
     '/about/1135135.jpg',
     '/about/photo-1518546305927-5a555bb7020d.avif',
     '/about/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+    '/about/the-many-poses-of-alonsos-post-race-celebrations-during-his-v0-lhl9lg39dk7b1.jpg',
     '/about/2021-08-05.jpg',
+    '/about/Stake-com-logo-high-res.jpg',
     '/about/downtown-medellin-colombia.avif',
     '/about/dice-chips-and-playing-cards.webp',
     '/about/a16ba0d0dc35df6649fcb42430fb0f9d.jpg',
-    '/about/merlin_138870675_80a610d6-d7a6-4bcc-aa2d-cbe76d7fadf7-articleLarge.webp'
+    '/about/air-jordan-logo-667x500.jpg',
+    '/about/merlin_138870675_80a610d6-d7a6-4bcc-aa2d-cbe76d7fadf7-articleLarge.webp',
+    '/about/5857823720_f4eb48c9a2_b.jpg',
+    '/about/2025-porsche-911-turbo-102-668566873f882.avif',
+    '/about/A-Lagunitas-IPA-at-the-brewery-in-Petaluma-California..jpeg',
+    '/about/Lewis-Hamilton-Image-Twitter-X-FabrizioRomano.webp',
   ]
 
   useEffect(() => {
@@ -89,33 +99,41 @@ export default function About() {
         <div className={`mb-[5rem] ${loading ? 'hidden' : 'block'}`}>
           <h1 className="uppercase font-bold project-name leading-[100%] text-[40px] lg:text-[100px] mb-10 lg:mb-[3.5rem]">About Me</h1>
           <p className="citation font-[300] tracking-wide text-2xl italic">
-            Hey there! I&rsquo;m Kevin Lopez, a 29-year-old front-end developer based in the vibrant city of Boston, MA. Born and raised in the USA, my roots trace back to the beautiful landscapes and rich culture of Colombia, infusing my work with a blend of American innovation and Latin warmth.
+            Hi, I&rsquo;m Kevin Lopez, a full-stack developer based in Boston, MA. I&rsquo;m passionate about building clean, user-friendly web experiences from the ground up—whether it&rsquo;s crafting sleek front-end designs or working on back-end functionality.
             <br />
             <br />
-            With a passion for crafting engaging and user-friendly web experiences, I thrive on the creative challenges of front-end development. From sleek designs to seamless functionality, I strive to bring visions to life through clean code and intuitive interfaces.
+            Outside of coding, you&rsquo;ll probably catch me gaming, listening to music, keeping up with sports, checking out cars, or trying my luck with a little gambling and investing in stocks & crypto. I&rsquo;m always up for good times and new challenges.
             <br />
             <br />
-            When Im not immersed in the digital realm, you&rsquo;ll often find me exploring Boston&rsquo;s eclectic neighborhoods, sipping on a freshly brewed beer, or losing track of time while jamming to my favorite tunes. Scroll below to discover some of my favorite music and snapshots.
-            <br />
-            <br />
-            Let&rsquo;s connect and build something incredible!
+            Take a look around, and if you&rsquo;re interested in collaborating, let&rsquo;s connect!
           </p>
         </div>
         <div className={`flex justify-center items-center h-[50vh] ${loading ? 'block' : 'hidden'}`}>
           <div className="animate-spin rounded-full h-32 w-32 border-b-4"></div>
         </div>
-        <div className={`flex flex-col lg:flex-row justify-between lg:gap-4 ${loading ? 'hidden' : 'block'}`}>
+        {/* <div className={`flex flex-col lg:flex-row justify-between lg:gap-4 ${loading ? 'hidden' : 'block'}`}>
           <iframe className={loading ? "soundcloud-embed mb-5 w-[100%] rounded-lg" : "soundcloud-embed mb-5 w-[100%] rounded-lg fade-in active"}  width="100%" height="315" src="https://www.youtube.com/embed/videoseries?si=jo6nZlwAlcZa6U5e&amp;list=PL3ViwmNp6G049Twt9ALIeOq7S17WxA62z" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        </div>
+        </div> */}
         <div className={`flex flex-col lg:flex-row justify-between lg:gap-4 ${loading ? 'hidden' : 'block'}`}>
-          <iframe height="275" allow="autoplay" className={loading ? "soundcloud-embed mb-5 w-[100%] rounded-lg" : "soundcloud-embed mb-5 w-[100%] rounded-lg fade-in active"} src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/141521022&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"  ></iframe>
-          <iframe height="275" allow="autoplay" className={loading ? "soundcloud-embed mb-5 w-[100%] rounded-lg" : "soundcloud-embed mb-5 w-[100%] rounded-lg fade-in active"} src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/335930055&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"  ></iframe>
-          <iframe height="275" allow="autoplay" className={loading ? "soundcloud-embed mb-5 w-[100%] rounded-lg" : "soundcloud-embed mb-5 w-[100%] rounded-lg fade-in active"} src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/346202223&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"  ></iframe>
-        </div>
+          <iframe style={{borderRadius: '.5rem', marginBottom: '1rem'}} src="https://open.spotify.com/embed/playlist/37i9dQZF1E4zk5SV0SRQSB?utm_source=generator" width="99%" height="352" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+          <iframe style={{borderRadius: '.5rem', marginBottom: '1rem'}} src="https://open.spotify.com/embed/artist/3TVXtAsR1Inumwj472S9r4?utm_source=generator" width="99%" height="352" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+          <iframe style={{borderRadius: '.5rem', marginBottom: '1rem'}}   src="https://open.spotify.com/embed/playlist/37i9dQZF1E4zglX5vhD6vJ?utm_source=generator" width="99%" height="352" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>        </div>
         <div className={`columns-2 gap-4 md:columns-3 ${loading ? 'hidden' : 'block'}`}>
           {images.map((url, index) => 
+            url === '/about/Stake-com-logo-high-res.jpg' || url === '/about/300x600.gif' ? 
+            <Link key={index} href="https://stake.us/?c=Kevinlo51" target="_blank">
+              <img 
+                key={index}
+                src={url}
+                loading="lazy"
+                alt="lifestyle picture"
+                ref={el => imagesRef.current[index] = el}
+                className="w-full aspect-square mb-4 max-w-full rounded-lg object-cover object-center fade-in"
+              />
+            </Link>
+            :
             <img 
-              key={url}
+              key={index}
               src={url}
               loading="lazy"
               alt="lifestyle picture"
--- src/app/components/HomePageHero.js
+++ src/app/components/HomePageHero.js
@@ -1,6 +1,49 @@
+"use client";
 
+import { useEffect } from "react";
+import gsap from "gsap";
 
-export default function HomePageHero () {
+export default function HomePageHero() {
+  useEffect(() => {
+    const letters = document.querySelectorAll(".animated-letter"); // Select all letter spans
+
+    gsap.fromTo(
+      letters,
+      {
+        opacity: 0, 
+        rotation: 360,
+        y: -20,
+      },
+      {
+        duration: 1, 
+        opacity: 1,
+        rotation: 0,
+        y: 0,
+        stagger: {
+          amount: 1.5,
+          from: "start", 
+        },
+        ease: "power1.out",
+      }
+    );
+  }, []);
+
+  const text = "Frontend Developer";
+  const animatedText = (
+    <>
+      {text.split(' ').map((word, index) => (
+        <span key={index}>
+          {word.split('').map((letter, letterIndex) => (
+            <span key={`${index}-${letterIndex}`} className="animated-letter">
+              {letter === ' ' ? '\u00A0' : letter}
+            </span>
+          ))}
+          {index === 0 && <br />}
+        </span>
+      ))}
+    </>
+  );
+
   return (
     <section className="flex items-center h-screen relative px-[30px] lg:px-[60px]">
       <div className="absolute z-50 -translate-y-1/2 top-1/2 right-0 rotate-[270deg] translate-x-[36.5%] text-center flex items-center">
@@ -9,24 +52,23 @@ export default function HomePageHero () {
         </a>
       </div>
       <h1 className="overflow-hidden text-heading font-bold text-[45px] leading-[100%] xl:text-[120px] 2xl:text-[125px] xl:leading-[120px] 2xl:leading-[140px] uppercase mb-16">
-        Frontend Developer
+        {animatedText}
       </h1>
       <div className="absolute left-0 bottom-[60px] right-0 flex items-end lg:items-start justify-between px-[30px] lg:px-[60px]">
         <div className="xl:h-[100px] xl:w-[100px] 2xl:w-[125px] 2xl:h-[125px] flex items-center">
           <div className="animate-bounce">
             <svg className="translate-y-12 lg:translate-y-4" width="50px" height="100px" viewBox="0 0 80 40">
-              <polyline fill="none" strokeWidth="4" strokeLinecap="inherit" strokeLinejoin="inherit" points="
-            0,0 40,20 80,0"/>
+              <polyline fill="none" strokeWidth="4" strokeLinecap="inherit" strokeLinejoin="inherit" points="0,0 40,20 80,0" />
             </svg>
           </div>
         </div>
         <div className="flex flex-col mb-2 lg:mb-0">
-          <div className="flex items-center gap-4 xl:gap-[20px] 2xl:gap-[30px] down" >
+          <div className="flex items-center gap-4 xl:gap-[20px] 2xl:gap-[30px] down">
             <h3 className="uppercase xl:text-[26px] 2xl:text-[32px] font-[400]">Open to work</h3>
           </div>
-          <h4 className="font-[200] lg:ml-1 based-in" >Based in Boston, MA</h4>
+          <h4 className="font-[200] lg:ml-1 based-in">Based in Boston, MA</h4>
         </div>
       </div>
     </section>
-  )
+  );
 }
